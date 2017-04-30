7a58e69e1e19982629382d54d021200da4283953
libert <845301110@qq.com>
2017-04-30 14:07:54 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-<?php// +----------------------------------------------------------------------// | ThinkPHP [ WE CAN DO IT JUST THINK ]// +----------------------------------------------------------------------// | Copyright (c) 2006-2014 http://thinkphp.cn All rights reserved.// +----------------------------------------------------------------------// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )// +----------------------------------------------------------------------// | Author: liu21st <liu21st@gmail.com>// +----------------------------------------------------------------------// 应用入口文件// 检测PHP环境if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');// 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false//define('APP_DEBUG',true);// 定义应用目录define('APP_PATH','./App/');if(!is_file('Data/install.lock')){    header('Location: /install.php');    exit;}// 引入ThinkPHP入口文件require './ThinkPHP/ThinkPHP.php';// 亲^_^ 后面不需要任何代码了 就是如此简单
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2014 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+// 应用入口文件
+// 检测PHP环境
+if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+// 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
+// define('APP_DEBUG',true);
+// 定义应用目录
+define('APP_PATH','./App/');
+if(!is_file('Data/install.lock')){    header('Location: /install.php');    exit;}
+// 引入ThinkPHP入口文件
+require './ThinkPHP/ThinkPHP.php';
